e680f947b67eaad01d14501d46616cc4c54b8a84
Renzo Visperas <renzovisperas07@gmail.com>
2022-09-04 21:32:27 +0800

    add heading

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,10 +1,13 @@
 import logo from "assets/images/logo-light.jpg";
+import styles from "./Login.module.scss";
 
 const Login = () => {
   return (
-    <main>
+    <main className={styles.main}>
       <img src={logo} alt="logo" />
-      <h1>Login</h1>
+      <h1 className={styles.heading}>
+        Vote for the <span>Best Candidate</span> & Discover peace
+      </h1>
     </main>
   );
 };
