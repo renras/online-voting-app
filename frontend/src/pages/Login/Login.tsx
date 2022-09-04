321a68b218ae8a949afcfcb0311394a0a043b826
Renzo Visperas <renzovisperas07@gmail.com>
2022-09-04 22:00:49 +0800

    initialize layout

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,13 +1,22 @@
 import logo from "assets/images/logo-light.jpg";
+import pollingPlace from "assets/images/polling-place.jpg";
 import styles from "./Login.module.scss";
 
 const Login = () => {
   return (
     <main className={styles.main}>
-      <img src={logo} alt="logo" />
-      <h1 className={styles.heading}>
-        Vote for the <span>Best Candidate</span> & Discover peace
-      </h1>
+      <img src={logo} alt="logo" className={styles.logo} />
+      <div className={styles.leftColumn}>
+        <h1 className={styles.heading}>
+          Vote for the <span>Best Candidate</span> & Discover peace
+        </h1>
+        <img
+          src={pollingPlace}
+          alt="polling place"
+          className={styles.pollingImg}
+        />
+      </div>
+      <form className={styles.form}>Form</form>
     </main>
   );
 };
