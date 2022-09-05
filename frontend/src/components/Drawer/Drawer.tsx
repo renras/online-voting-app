e35823bdbb11f51dbe8b7f7b4976d0267929d02b
Renzo Visperas <renzovisperas07@gmail.com>
2022-09-05 22:32:17 +0800

    add nav links in drawer

--- frontend/src/components/Drawer/Drawer.tsx
+++ frontend/src/components/Drawer/Drawer.tsx
@@ -1,10 +1,32 @@
 import styles from "./Drawer.module.scss";
 import logo from "assets/images/logo-light.png";
+import iconElection from "assets/icons/icon-election.svg";
+import iconHome from "assets/icons/icon-home.svg";
+import iconResults from "assets/icons/icon-results.svg";
+import iconVoters from "assets/icons/icon-voters.svg";
 
 const Drawer = () => {
   return (
     <aside className={styles.drawer}>
       <img src={logo} alt="logo" />
+      <nav className={styles.nav}>
+        <a href="/">
+          <img src={iconHome} alt="home icon" />
+          Home
+        </a>
+        <a href="/election">
+          <img src={iconElection} alt="election icon" />
+          Election
+        </a>
+        <a href="/voters">
+          <img src={iconVoters} alt="voters icon" />
+          Voters
+        </a>
+        <a href="results">
+          <img src={iconResults} alt="results icon" />
+          Results
+        </a>
+      </nav>
     </aside>
   );
 };
