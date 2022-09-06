592266a0f221ca997735b84291a0f18c89f37960
Kristan Visperas <renzovisperas07@gmail.com>
2022-09-06 13:48:42 +0800

    adjust login layout

--- frontend/src/components/Layout/LoginLayout/LoginLayout.tsx
+++ frontend/src/components/Layout/LoginLayout/LoginLayout.tsx
@@ -11,17 +11,19 @@ const LoginLayout = ({ children }: Props) => {
   return (
     <main className={styles.main}>
       <img src={logo} alt="logo" className={styles.logo} />
-      <div className={styles.leftColumn}>
-        <h1 className={styles.heading}>
-          Vote for the <span>Best Candidate</span> & Discover peace
-        </h1>
-        <img
-          src={pollingPlace}
-          alt="polling place"
-          className={styles.pollingImg}
-        />
+      <div className={styles.content}>
+        <div className={styles.leftColumn}>
+          <h1 className={styles.heading}>
+            Vote for the <span>Best Candidate</span> & Discover peace
+          </h1>
+          <img
+            src={pollingPlace}
+            alt="polling place"
+            className={styles.pollingImg}
+          />
+        </div>
+        {children}
       </div>
-      {children}
     </main>
   );
 };
