a1261f770829f6896dcab9ce81941a6d34a4dc5d
Kristan Visperas <renzovisperas07@gmail.com>
2022-09-06 19:53:25 +0800

    add card content

--- frontend/src/pages/Home/Card/Card.tsx
+++ frontend/src/pages/Home/Card/Card.tsx
@@ -1,4 +1,5 @@
 import styles from "./Card.module.scss";
+import Button from "components/Button/Button";
 
 interface Props {
   name: string;
@@ -8,7 +9,13 @@ const Card = ({ name }: Props) => {
   return (
     <article className={styles.container}>
       <div className={styles.img}></div>
-      <p>{name}</p>
+
+      <div className={styles.content}>
+        <p className={styles.name}>{name}</p>
+        <Button size="small" className={styles.button}>
+          Vote
+        </Button>
+      </div>
     </article>
   );
 };
