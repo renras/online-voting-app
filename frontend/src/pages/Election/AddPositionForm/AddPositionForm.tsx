932dc7dfef734b2cb188feaea68a9b81d141d89f
Kristan Visperas <renzovisperas07@gmail.com>
2022-09-07 20:50:35 +0800

    add title input and submit button in form

--- frontend/src/pages/Election/AddPositionForm/AddPositionForm.tsx
+++ frontend/src/pages/Election/AddPositionForm/AddPositionForm.tsx
@@ -1,5 +1,23 @@
+import Input from "components/Input/Input";
+import styles from "./AddPositionForm.module.scss";
+import Label from "components/Label/Label";
+import Button from "components/Button/Button";
+import plusIcon from "assets/icons/icon-plus.svg";
+
 const AddPositionForm = () => {
-  return <div>Add Position Form</div>;
+  return (
+    <form className={styles.form}>
+      <h1 className={styles.heading}>Add New Position</h1>
+      <Label htmlFor="title" className={styles.label}>
+        Title
+      </Label>
+      <Input id="title" className={styles.input} />
+      <Button className={styles.button} size="large">
+        <img src={plusIcon} alt="plus icon" />
+        Add Position
+      </Button>
+    </form>
+  );
 };
 
 export default AddPositionForm;
