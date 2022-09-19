ccf85a6d37ab19e2dbf7747acf63964f1a93a401
Renzo Visperas <renzovisperas07@gmail.com>
2022-09-19 21:14:24 +0800

    add users to database

---
+++ backend/Models/User.php
@@ -0,0 +1,100 @@
+<?php
+class User {
+  private $conn;
+  private $table = 'users';
+
+  public $id;
+  public $username;
+  public $password;
+
+  public function __construct($db) {
+    $this->conn = $db;
+  }
+
+  public function read() {
+    $query = 'SELECT * FROM ' . $this->table;
+
+    $stmt = $this->conn->prepare($query);
+
+    $stmt->execute();
+
+    return $stmt;
+  }
+
+  public function read_single() {
+    $query = 'SELECT * FROM ' . $this->table . ' WHERE id = ? LIMIT 0,1';
+
+    $stmt = $this->conn->prepare($query);
+
+    $stmt->bindParam(1, $this->id);
+
+    $stmt->execute();
+
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    $this->id = $row['id'];
+    $this->username = $row['username'];
+    $this->password = $row['password'];
+  }
+
+  public function create() {
+    $query = 'INSERT INTO ' . $this->table . ' SET username = :username, password = :password';
+
+    $stmt = $this->conn->prepare($query);
+
+    $this->username = htmlspecialchars(strip_tags($this->username));
+    $this->password = htmlspecialchars(strip_tags($this->password));
+
+    $stmt->bindParam(':username', $this->username);
+    $stmt->bindParam(':password', $this->password);
+
+    if($stmt->execute()) {
+      return true;
+    }
+
+    printf("Error: %s.\n", $stmt->error);
+
+    return false;
+  }
+
+  public function update() {
+    $query = 'UPDATE ' . $this->table . ' SET username = :username, password = :password WHERE id = :id';
+
+    $stmt = $this->conn->prepare($query);
+
+    $this->username = htmlspecialchars(strip_tags($this->username));
+    $this->password = htmlspecialchars(strip_tags($this->password));
+    $this->id = htmlspecialchars(strip_tags($this->id));
+
+    $stmt->bindParam(':username', $this->username);
+    $stmt->bindParam(':password', $this->password);
+    $stmt->bindParam(':id', $this->id);
+
+    if($stmt->execute()) {
+      return true;
+    }
+
+    printf("Error: %s.\n", $stmt->error);
+
+    return false;
+  }
+
+  public function delete() {
+    $query = 'DELETE FROM ' . $this->table . ' WHERE id = :id';
+
+    $stmt = $this->conn->prepare($query);
+
+    $this->id = htmlspecialchars(strip_tags($this->id));
+
+    $stmt->bindParam(':id', $this->id);
+
+    if($stmt->execute()) {
+      return true;
+    }
+
+    printf("Error: %s.\n", $stmt->error);
+
+    return false;
+  }
+}
+?>
---
+++ backend/api/users/index.php
@@ -0,0 +1,114 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: *');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+require_once('../../Config/Database.php');
+require_once('../../Models/User.php');
+
+// get request
+if($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $database = new Database();
+    $db = $database->connect();
+
+    $user = new User($db);
+    $result = $user->read();
+    $num = $result->rowCount();
+
+    if($num > 0) {
+        $users_arr = array();
+
+        while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            extract($row);
+
+            $user_item = array(
+                'id' => $id,
+                'username' => $username,
+                'password' => $password
+            );
+
+            array_push($users_arr, $user_item);
+        }
+
+        echo json_encode($users_arr);
+    } else {
+        echo json_encode(
+            array('message' => 'No Users Found')
+        );
+    }
+}
+
+// post request
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $database = new Database();
+    $db = $database->connect();
+
+    $user = new User($db);
+
+    $data = json_decode(file_get_contents("php://input"));
+
+    $user->username = $data->username;
+    $user->password = $data->password;
+
+    if($user->create()) {
+        echo json_encode(
+            array('message' => 'User Created')
+        );
+    } else {
+        echo json_encode(
+            array('message' => 'User Not Created')
+        );
+    }
+}
+
+// put request
+if($_SERVER['REQUEST_METHOD'] == 'PUT') {
+    $database = new Database();
+    $db = $database->connect();
+
+    $user = new User($db);
+
+    $data = json_decode(file_get_contents("php://input"));
+
+    $user->id = $data->id;
+    $user->username = $data->username;
+    $user->password = $data->password;
+
+    if($user->update()) {
+        echo json_encode(
+            array('message' => 'User Updated')
+        );
+    } else {
+        echo json_encode(
+            array('message' => 'User Not Updated')
+        );
+    }
+}
+
+// delete request
+if($_SERVER['REQUEST_METHOD'] == 'DELETE') {
+    $database = new Database();
+    $db = $database->connect();
+
+    $user = new User($db);
+
+    $data = json_decode(file_get_contents("php://input"));
+
+    $user->id = $data->id;
+
+    if($user->delete()) {
+        echo json_encode(
+            array('message' => 'User Deleted')
+        );
+    } else {
+        echo json_encode(
+            array('message' => 'User Not Deleted')
+        );
+    }
+}
+
+
+?>
