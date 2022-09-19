f1350f1a741ea54d46cfb8844bee63df886338fe
Renzo Visperas <renzovisperas07@gmail.com>
2022-09-19 21:48:41 +0800

    integrate react hook form to user registration

--- frontend/src/pages/Register/Register.tsx
+++ frontend/src/pages/Register/Register.tsx
@@ -4,13 +4,23 @@ import Input from "components/Input/Input";
 import Button from "components/Button/Button";
 import { useLocation } from "react-router-dom";
 import LoginLayout from "components/Layout/LoginLayout/LoginLayout";
+import { useForm } from "react-hook-form";
+
+interface FormData {
+  username: string;
+  password: string;
+  confirm_password: string;
+}
 
 const Login = () => {
   const { pathname } = useLocation();
+  const { register, handleSubmit } = useForm<FormData>();
+
+  const onSubmit = handleSubmit(async (data) => console.log(data));
 
   return (
     <LoginLayout>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={onSubmit}>
         <div className={styles.headings}>
           <a
             href="/login"
@@ -30,19 +40,33 @@ const Login = () => {
         <Label htmlFor="username" className={styles.label}>
           Username
         </Label>
-        <Input id="username" className={styles.input} />
+        <Input
+          id="username"
+          className={styles.input}
+          {...register("username", { required: true })}
+        />
 
         {/* password */}
         <Label htmlFor="password" className={styles.label}>
           Password
         </Label>
-        <Input id="password" type="password" className={styles.input} />
+        <Input
+          id="password"
+          type="password"
+          className={styles.input}
+          {...register("password", { required: true })}
+        />
 
         {/* confirm password */}
-        <Label htmlFor="confirm-password" className={styles.label}>
+        <Label htmlFor="confirm_password" className={styles.label}>
           Confirm Password
         </Label>
-        <Input id="confirm-password" type="password" className={styles.input} />
+        <Input
+          id="confirm_password"
+          type="password"
+          className={styles.input}
+          {...register("confirm_password", { required: true })}
+        />
 
         <Button className={styles.button} fluid size="large">
           Register
